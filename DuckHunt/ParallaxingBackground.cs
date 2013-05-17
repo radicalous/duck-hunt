3d5752e7d2f7743292ab16be690c8841e3df4882
pennerchang@gmail.com <pennerchang@gmail.com@70054abf-394d-4c61-71fe-93512722c465>
2013-05-17 01:16:14 +0000

    1. add some grass imgs 2. Implement global view and local view. So all model will run in a global view. controler or view object will conver the global position to local view. With this design, later we can support multiple player with different screen resolution and devices with different screen resolution will got same pictures.

--- DuckHunt/ParallaxingBackground.cs
+++ DuckHunt/ParallaxingBackground.cs
@@ -150,13 +150,15 @@ namespace DuckHuntCommon
 
         int _screenWidth = 0;
         int _screenHeight = 0;
+        Vector2 _orgpos;
 
+        float scale = 1.0f;
 
-        public void Initialize(Texture2D texture1, int screenWidth, int screenHeight, int speed)
+        public void Initialize(Texture2D texture1, Vector2 orgpos, int screenWidth, int screenHeight, int speed)
         {
+            _orgpos = orgpos;
 
-
-            _screenWidth = screenHeight;
+            _screenWidth = screenWidth;
             _screenHeight = screenHeight;
 
             // Load the background texture we will be using
@@ -174,20 +176,34 @@ namespace DuckHuntCommon
 
 
             // If we divide the screen with the texture width then we can determine the number of tiles need.
+            scale = _screenHeight * 1.0f / texture.Height;
 
             // We add 1 to it so that we won't have a gap in the tiling
 
-            positions = new Vector2[screenWidth / texture.Width + 1];
+            positions = new Vector2[screenWidth / (int)(texture.Width*scale) + 1];
 
             // Set the initial positions of the parallaxing background
 
-            for (int i = 0; i < positions.Length; i++)
+
+            if (scale * texture.Width < _screenWidth)
             {
 
-                // We need the tiles to be side by side to create a tiling effect
+                for (int i = 0; i < positions.Length; i++)
+                {
 
-                positions[i] = new Vector2(i * texture.Width, 0);
-                positions[i].Y = _screenHeight - texture.Height;
+                    // We need the tiles to be side by side to create a tiling effect
+
+                    positions[i] = new Vector2(i * texture.Width * scale, 0);
+                    positions[i].Y = 0;
+                    positions[i] += orgpos;
+
+                }
+            }
+            else
+            {
+                positions[0].X = -((scale * texture.Width - _screenWidth) / 2);
+                positions[0].Y = 0;
+                positions[0] += orgpos;
 
             }
 
@@ -209,13 +225,47 @@ namespace DuckHuntCommon
             Vector2 pos = new Vector2(0, 0);
 
 
+
             for (int i = 0; i < positions.Length; i++)
             {
 
-                Rectangle rectBg = new Rectangle((int)positions[i].X, (int)positions[i].Y, bgWidth, bgHeight);
+                int width = bgWidth;
+                int height = bgHeight;
+ 
+                Rectangle textrc = new Rectangle(0, 0, width, height);
+                Rectangle dstrc = new Rectangle();
 
-                spriteBatch.Draw(texture, positions[i], null, Color.White,
-                    0, pos, 1.0F, SpriteEffects.None, depth);// depth = 1, back
+                if (positions[i].X < _orgpos.X)
+                {
+
+                    textrc.X = (int)((_orgpos.X - positions[i].X) * 1.0 / ((_orgpos.X - positions[i].X) * 2 + _screenWidth) * textrc.Width);
+                    textrc.Width = bgWidth - textrc.X * 2;
+
+                    //
+                    dstrc.X = (int)_orgpos.X;
+                    dstrc.Width = (int)(textrc.Width * scale);
+                    dstrc.Height = _screenHeight;
+                }
+                else
+                {
+                    if ((int)positions[i].X + bgWidth * scale > _screenWidth + _orgpos.X)
+                    {
+                        width = (int)((_screenWidth + _orgpos.X - positions[i].X) / scale);
+                    }
+                    textrc.Width = width;
+
+                    dstrc.X = (int)positions[i].X;
+                    dstrc.Width = (int)(textrc.Width * scale);
+                    dstrc.Height = _screenHeight;
+                }
+
+
+                //spriteBatch.Draw(texture, positions[i], textrc, Color.White,
+                //    0, pos, scale, SpriteEffects.None, depth);// depth = 1, back
+                //spriteBatch.Draw(texture, dstrc, textrc, Color.White);
+                Vector2 orgion = new Vector2(0, 0);
+                spriteBatch.Draw(texture, dstrc, textrc, Color.White, 0, orgion, SpriteEffects.None, depth); // 0 f
+
 
                 //spriteBatch.Draw(texture, rectBg, Color.White);
 
